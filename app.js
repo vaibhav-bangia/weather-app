135b4923da92986dd261fbfd2c80415408e14902
Vaibhav Bangia <vaibhavbangia@gmail.com>
2021-07-06 23:33:11 +0530

    UI set
    Basic UI set and working fine

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const request = require('request');
+const express = require('express')
+const bodyParser = require('body-parser')
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
+const app = express()
+const port = 3000
 
-address = 'Rajouri Garden'
-geocode(address, (error, data) => {
+app.get('/', (req, res) => res.sendFile(__dirname+ '/index.html'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/",function(req,res){
+    var loc = req.body.loc;
+    address = loc; 
+    console.log(loc)
+    geocode(address, (error, data) => {
     if(error){
         return console.log(error);
     }
@@ -15,4 +23,20 @@ geocode(address, (error, data) => {
         console.log(`it is currently ${forecastData.cur_temp} C in ${data.locaton}`)
     });
 })
+    res.send("HelO!");
+})
 
+// geocode(address, (error, data) => {
+//     if(error){
+//         return console.log(error);
+//     }
+    
+//     forecast({ latitude: data.latitude, longitude: data.longitude }, (error, forecastData) => {
+//         if(error){
+//             return console.log(error);
+//         }
+//         console.log(`it is currently ${forecastData.cur_temp} C in ${data.locaton}`)
+//     });
+// })
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
