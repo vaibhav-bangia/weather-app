5ef2a96d3d7bd8f5b0ca017506c7d2006aa4f9a4
Vaibhav Bangia <vaibhavbangia@gmail.com>
2021-07-17 23:01:32 +0530

    /

--- app.js
+++ app.js
@@ -4,48 +4,34 @@ const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 const app = express()
 const port = 3000
-var x="wtf"; 
+var x = "temp"
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.sendFile(__dirname+ '/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/",function(req,res){
+app.post("/", function (req, res) {
     var loc = req.body.loc;
-    address = loc; 
+    address = loc;
     console.log(loc)
     geocode(address, (error, data) => {
-    if(error){
-        return console.log(error);
-    }
-    
-    forecast({ latitude: data.latitude, longitude: data.longitude }, (error, forecastData) => {
-        if(error){
+        if (error) {
             return console.log(error);
         }
-        x = forecastData.cur_temp;
-        console.log(`It is currently ${forecastData.cur_temp} C in ${data.locaton}`)
-        res.render("result",{temp:x,location:address})
-    });
-})
-    
-    //res.send("HelO!");
-})
 
-// geocode(address, (error, data) => {
-//     if(error){
-//         return console.log(error);
-//     }
-    
-//     forecast({ latitude: data.latitude, longitude: data.longitude }, (error, forecastData) => {
-//         if(error){
-//             return console.log(error);
-//         }
-//         console.log(`it is currently ${forecastData.cur_temp} C in ${data.locaton}`)
-//     });
-// })
+        forecast({ latitude: data.latitude, longitude: data.longitude }, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            x = forecastData.cur_temp;
+            console.log(`It is currently ${forecastData.cur_temp} C in ${data.locaton}`)
+            res.render("result", { temp: x, location: address })
+        });
+    })
 
+
+})
 app.listen(port, () => console.log(`Example app listening on port port!`))
